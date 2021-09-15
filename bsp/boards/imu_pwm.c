02fecd321f419c9f0ae22960573cf06e8470ea96
qiayuan <liaoqiayuan@gmail.com>
2021-09-15 23:10:59 +0800

    Add bsp imu

---
+++ bsp/boards/imu_pwm.c
@@ -0,0 +1,7 @@
+#include "imu_pwm.h"
+
+#include "main.h"
+
+void imu_pwm_set(uint16_t pwm) {
+    TIM10->CCR1 = (pwm);
+}
---
+++ bsp/boards/struct_typedef.h
@@ -0,0 +1,10 @@
+#ifndef STRUCT_TYPEDEF_H
+#define STRUCT_TYPEDEF_H
+
+#include <stdint.h>
+
+typedef unsigned char bool_t;
+typedef float fp32;
+typedef double fp64;
+
+#endif
